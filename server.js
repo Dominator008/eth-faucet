336fae89e53bffc6f2fd9ff0c0ba6f347706420c
Michael Zhou <zhoumotongxue008@gmail.com>
2018-10-09 21:35:55 -0700

    Implement stricter anti-abuse measure

--- server.js
+++ server.js
@@ -15,18 +15,21 @@ const rpcWrapperEngine = require('./index.js');
 const regularPageCode = require('fs').readFileSync('./index.html', 'utf-8');
 const mascaraPageCode = require('fs').readFileSync('./zero.html', 'utf-8');
 const pageCode = MASCARA_SUPPORT ? mascaraPageCode : regularPageCode;
+const cache = require('memory-cache');
 
 const ETHER = 1e18;
-const faucetAmountWei = (1 * ETHER);
+const FIFTEEN_MIN = 15 * 60 * 1000;
+const FAUCET_AMOUNT = (1 * ETHER);
+const TEN_ETH = new BN('10000000000000000000', 10);
 
 console.log('Acting as faucet for address:', config.address);
 
 // Lazy nonce tracking fix:
 // Force an exit after ten minutes (docker will trigger a restart)
-setTimeout(() => {
-  console.log('Restarting for better nonce tracking');
-  process.exit();
-}, 10 * 60 * 1000);
+// setTimeout(() => {
+//   console.log('Restarting for better nonce tracking');
+//   process.exit();
+// }, 10 * 60 * 1000);
 
 //
 // create engine
@@ -75,11 +78,9 @@ function startServer(appCode) {
   // add IP-based rate limiting
   app.post('/', new RateLimit({
              // 15 minutes
-             windowMs: 15 * 60 * 1000,
+             windowMs: FIFTEEN_MIN,
              // limit each IP to N requests per windowMs
              max: 200,
-             // disable delaying - full speed until the max limit is reached
-             delayMs: 0,
            }));
   // the fauceting request
   app.post('/', function(req, res) {
@@ -89,15 +90,24 @@ function startServer(appCode) {
       targetAddress = '0x' + targetAddress;
     }
     if (targetAddress.length !== 42) {
-      return didError(new Error('Address parse failure - ' + targetAddress));
+      didError(new Error('Address parse failure - ' + targetAddress));
+      return;
     }
 
+    const greedy = new Error('User is greedy.');
+    const cachedBalance = cache.get(targetAddress);
+    if (cachedBalance && cachedBalance.gt(TEN_ETH)) {
+      didError(greedy);
+      return;
+    }
     // check for greediness
     ethQuery.getBalance(targetAddress, 'pending')
         .then(function(balance) {
-          const balanceTooFull = balance.gt(new BN('10000000000000000000', 10));
+          const balanceTooFull = balance.gt(TEN_ETH);
           if (balanceTooFull) {
-            return didError(new Error('User is greedy.'));
+            cache.put(targetAddress, balance, FIFTEEN_MIN);
+            didError(greedy);
+            return;
           }
 
           ethQuery.gasPrice()
@@ -108,12 +118,16 @@ function startServer(appCode) {
                     .sendTransaction({
                       to: targetAddress,
                       from: config.address,
-                      value: faucetAmountWei,
+                      value: FAUCET_AMOUNT,
                       data: '',
                       gasPrice: adjustedPrice
                     })
                     .then(function(result) {
                       console.log('sent tx:', result);
+                      cache.put(
+                          targetAddress,
+                          balance.add(new BN(String(FAUCET_AMOUNT), 10)),
+                          FIFTEEN_MIN);
                       res.send(result);
                     })
                     .catch(didError);
